f36e763e2f7ac6dc549b59d8f4fba660ad45ac93
Александр <102449824+phyalexander@users.noreply.github.com>
2024-10-04 23:07:12 +0300

    Update install_saturn.sh
    fixed bug with dot

--- install_saturn.sh
+++ install_saturn.sh
@@ -9,7 +9,7 @@ SCRIPT_DIR=$(dirname "\$0")
 # Копируем файл "jars/Saturn.jar", папку "language/std" и исполняемый скрипт в директорию ".Saturn"
 cp -r "$SCRIPT_DIR/jars/Saturn.jar" "$SCRIPT_DIR/language/std" ~/.Saturn/
 cp -r "$SCRIPT_DIR/saturn.sh" ~/.Saturn/
-chmod +x ~/Saturn/saturn.sh
+chmod +x ~/.Saturn/saturn.sh
 
 # Добавляем директорию ".Saturn" в PATH
 echo "export PATH=$PATH:~/.Saturn" > ~/.bashrc
