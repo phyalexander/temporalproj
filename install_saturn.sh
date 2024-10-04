c642a35df41b185a1242fe3927303b2efd9dcfc6
Александр <102449824+phyalexander@users.noreply.github.com>
2024-10-04 22:38:18 +0300

    Update install_saturn.sh
    updated for using saturh.sh

--- install_saturn.sh
+++ install_saturn.sh
@@ -6,9 +6,15 @@ mkdir -p ~/.Saturn
 # Определяем директорию, где находится сам скрипт
 SCRIPT_DIR=$(dirname "\$0")
 
-# Копируем файл "jars/Saturn.jar" и папку "language/std" в директорию ".Saturn"
+# Копируем файл "jars/Saturn.jar", папку "language/std" и исполняемый скрипт в директорию ".Saturn"
 cp -r "$SCRIPT_DIR/jars/Saturn.jar" "$SCRIPT_DIR/language/std" ~/.Saturn/
+cp -r "$SCRIPT_DIR/saturn.sh" ~/.Saturn/
+chmod +x ~/Saturn/saturn.sh
 
 # Добавляем директорию ".Saturn" в PATH
-echo "export PATH=$PATH:~/.Saturn" >> ~/.bashrc
-echo "export PATH=$PATH:~/.Saturn" >> ~/.zshrc
+echo "export PATH=$PATH:~/.Saturn" > ~/.bashrc
+echo "export PATH=$PATH:~/.Saturn" > ~/.zshrc
+
+# Удаляем git-репозиторий
+cd ..
+rm -rf Saturn
